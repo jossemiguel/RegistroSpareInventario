5176dc5c64eda636e30685efa607b3242d7e67bc
joseemiguel <josem860@hotmail.com>
2017-05-13 16:09:12 -0400

    RegistroSpare

---
+++ SistemaInventario/Controllers/RegistroSparesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaInventario.DAL;
+using SistemaInventario.Models;
+
+namespace SistemaInventario.Controllers
+{
+    public class RegistroSparesController : Controller
+    {
+        private InventarioDB db = new InventarioDB();
+
+        // GET: RegistroSpares
+        public ActionResult Index()
+        {
+            return View(db.Spare.ToList());
+        }
+
+        // GET: RegistroSpares/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegistroSpare registroSpare = db.Spare.Find(id);
+            if (registroSpare == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registroSpare);
+        }
+
+        // GET: RegistroSpares/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegistroSpares/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SpareID,Nombre,NumeroParte,SerialNumero,Almacen,Fecha")] RegistroSpare registroSpare)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Spare.Add(registroSpare);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(registroSpare);
+        }
+
+        // GET: RegistroSpares/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegistroSpare registroSpare = db.Spare.Find(id);
+            if (registroSpare == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registroSpare);
+        }
+
+        // POST: RegistroSpares/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SpareID,Nombre,NumeroParte,SerialNumero,Almacen,Fecha")] RegistroSpare registroSpare)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registroSpare).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registroSpare);
+        }
+
+        // GET: RegistroSpares/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegistroSpare registroSpare = db.Spare.Find(id);
+            if (registroSpare == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registroSpare);
+        }
+
+        // POST: RegistroSpares/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RegistroSpare registroSpare = db.Spare.Find(id);
+            db.Spare.Remove(registroSpare);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SistemaInventario/Models/RegistroSpare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SistemaInventario.Models
+{
+    public class RegistroSpare
+    {
+        [Key]
+
+        public int SpareID { get; set; }
+        public string Nombre { get; set; }
+        public string NumeroParte{ get; set; }
+        public string SerialNumero { get; set; }
+        public string Almacen { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+    }
+}
